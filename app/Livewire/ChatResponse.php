3e260e30184b858790bf5ade0b2de5f43e35dff7
Scott Larsen <sslarsen411@gmail.com>
2025-01-21 13:00:43 -0800

    Refactored AI chat to a trait

--- app/Livewire/ChatResponse.php
+++ app/Livewire/ChatResponse.php
@@ -2,13 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Traits\AIChat;
 use Illuminate\View\View;
 use Livewire\Component;
-use OpenAI\Laravel\Facades\OpenAI;
 
 class ChatResponse extends Component {
+    use AIChat;
+
     public array $helpText;
-    public $threadId;
+    public string $threadId;
     public $questionNo;
     //   public array $messages;
     public ?string $response = null;
@@ -30,48 +32,10 @@ class ChatResponse extends Component {
         $fname = session('cust.first_name');
         $prompt = <<<PROMPT
         This reviewer named $fname needs help with question number $this->questionNo
-
         Their request: {$this->helpText['content']}
 PROMPT;
-
-        $this->createMessage($prompt);
+        $this->createMessage($this->threadId, $prompt);
         return $this;
-    }
-
-    /**
-     * @param $inMessage
-     * @return void
-     */
-    public function createMessage($inMessage): void
-    {
-        OpenAI::threads()->messages()->create($this->threadId, [
-            'role' => 'user',
-            'content' => $inMessage,
-        ]);
-        $this->streamAiResponse();
-    }
-
-    /**
-     * @return void
-     */
-    public function streamAiResponse(): void
-    {
-        $stream = OpenAI::threads()->runs()->createStreamed(
-            threadId: $this->threadId,
-            parameters: [
-                'assistant_id' => config('openai.assistant'),
-            ]);
-
-        foreach ($stream as $content) {
-            if ($content->event == 'thread.message.delta') {
-                $this->stream(
-                    to: 'stream-'.$this->getId(),
-                    content: $content->response->delta->content[0]->text->value,
-                // replace: false
-                );
-                $this->response .= $content->response->delta->content[0]->text->value;
-            }
-        }
     }
 
     /**
---
+++ app/Traits/AIChat.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits;
+
+use OpenAI\Laravel\Facades\OpenAI;
+
+trait AIChat {
+    public string $threadId;
+    public ?string $response = null;
+
+    public function createMessage($inThreadId, $inMessage): void
+    {
+        $this->threadId = $inThreadId;
+        OpenAI::threads()->messages()->create($this->threadId, [
+            'role' => 'user',
+            'content' => $inMessage,
+        ]);
+        $this->streamAiResponse();
+    }
+
+    /**
+     * @return void
+     */
+    public function streamAiResponse(): void
+    {
+        $stream = OpenAI::threads()->runs()->createStreamed(
+            threadId: $this->threadId,
+            parameters: [
+                'assistant_id' => config('openai.assistant'),
+            ]);
+
+        foreach ($stream as $content) {
+            if ($content->event == 'thread.message.delta') {
+                $this->stream(
+                    to: 'stream-'.$this->getId(),
+                    content: $content->response->delta->content[0]->text->value,
+                );
+                $this->response .= $content->response->delta->content[0]->text->value;
+            }
+        }
+    }
+}
