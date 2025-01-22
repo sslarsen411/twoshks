68eede7783a0a6fdbf8c70fb3ab87d2f061dbb6f
Scott Larsen <sslarsen411@gmail.com>
2025-01-22 07:18:03 -0800

    Redid review-thank-you.blade.php

--- resources/views/email/review-thank-you.blade.php
+++ resources/views/email/review-thank-you.blade.php
@@ -1,57 +1,227 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        div {
-            margin-left: 2rem;
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Review Thank You</title>
+    <style media="all">
+        /* -------------------------------------
+        GLOBAL RESETS
+    ------------------------------------- */
+
+        body {
+            font-family: Helvetica, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            font-size: 16px;
+            line-height: 1.3;
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
         }
 
-        p, .review {
-            font-family: sans-serif;
-            font-size: 14px;
-            margin-bottom: 20px;
+        table {
+            border-collapse: separate;
+            mso-table-lspace: 0;
+            mso-table-rspace: 0;
+            width: 100%;
         }
 
-        .signature {
-            font-style: italic;
+        table td {
+            font-family: Helvetica, sans-serif;
+            font-size: 16px;
+            vertical-align: top;
         }
 
-        .red {
-            color: red;
+        /* -------------------------------------
+        BODY & CONTAINER
+    ------------------------------------- */
+
+        body {
+            background-color: #f4f5f6;
+            margin: 0;
+            padding: 0;
         }
+
+        .body {
+            background-color: #f4f5f6;
+            width: 100%;
+        }
+
+        .container {
+            margin: 0 auto !important;
+            max-width: 600px;
+            padding: 24px 0 0;
+            width: 600px;
+        }
+
+        .content {
+            box-sizing: border-box;
+            display: block;
+            margin: 0 auto;
+            max-width: 600px;
+            padding: 0;
+        }
+
+        /* -------------------------------------
+        HEADER, FOOTER, MAIN
+    ------------------------------------- */
+
+        .main {
+            background: #ffffff;
+            border: 1px solid #eaebed;
+            border-radius: 16px;
+            width: 100%;
+        }
+
+        .wrapper {
+            box-sizing: border-box;
+            padding: 24px;
+        }
+
+        .footer {
+            clear: both;
+            padding-top: 24px;
+            text-align: center;
+            width: 100%;
+        }
+
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+            color: #9a9ea6;
+            font-size: 16px;
+            text-align: center;
+        }
+
+        /* -------------------------------------
+        TYPOGRAPHY
+    ------------------------------------- */
+
+        p {
+            font-family: Helvetica, sans-serif;
+            font-size: 16px;
+            font-weight: normal;
+            margin: 0 0 16px;
+        }
+
+        a {
+            color: #0867ec;
+            text-decoration: underline;
+        }
+
+        /* -------------------------------------
+        OTHER STYLES THAT MIGHT BE USEFUL
+    ------------------------------------- */
+
+        .powered-by a {
+            text-decoration: none;
+        }
+
+        /* -------------------------------------
+        RESPONSIVE AND MOBILE FRIENDLY STYLES
+    ------------------------------------- */
+
+        @media only screen and (max-width: 640px) {
+            .main p,
+            .main td,
+            .main span {
+                font-size: 16px !important;
+            }
+
+            .wrapper {
+                padding: 8px !important;
+            }
+
+            .content {
+                padding: 0 !important;
+            }
+
+            .container {
+                padding: 0 !important;
+                padding-top: 8px !important;
+                width: 100% !important;
+            }
+
+            .main {
+                border-left-width: 0 !important;
+                border-radius: 0 !important;
+                border-right-width: 0 !important;
+            }
+        }
+
+        /* -------------------------------------
+        PRESERVE THESE STYLES IN THE HEAD
+    ------------------------------------- */
+        .apple-link a {
+            color: inherit !important;
+            font-family: inherit !important;
+            font-size: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
+            text-decoration: none !important;
+        }
+
+        #MessageViewBody a {
+            color: inherit;
+            text-decoration: none;
+            font-size: inherit;
+            font-family: inherit;
+            font-weight: inherit;
+            line-height: inherit;
+        }
+
     </style>
 </head>
 <body>
-<div>
-    <table role="presentation">
-        <tr>
-            <td style=" vertical-align: center">
-                <img src="https://cdn.mojoimpact.com/twoshakes/reviewguru-xs.webp" width="88" height="128"
-                     alt="The Rave Review Guru"
-                     style="display: inline; outline: 0; line-height: 100%; width: 160px; height: auto; max-width: 100%; border: 0;"/>
-            </td>
-            <td style="vertical-align: center">
-                <span
-                    style="font-size:2rem; font-family: ' Roboto', Arial, Helvetica, sans-serif; ">The Review Guru</span>
-            </td>
-        </tr>
-    </table>
-    <p>{{$first_name }}, here&apos;s a copy of your review for {{ $company }}:</p>
-    <div class="review"
-         style="width: 96%; margin-left: auto; margin-right:auto; margin-bottom:20px; padding:10px;
+<table role="presentation" class="body">
+    <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+            <div class="content">
+                <table role="presentation" class="main">
+                    <!-- START MAIN CONTENT AREA -->
+                    <tr>
+                        <td class="wrapper">
+                            <p>{{$first_name }}, here&apos;s a copy of your review for {{ $company }}:</p>
+                            <div class="review"
+                                 style="width: 96%; margin-left: auto; margin-right:auto; margin-bottom:20px; padding:10px;
         background-color:#DEDEDE;">
-        {{ $review }}
-    </div>
-    <p>
-        Thank you for using the Two Shakes App.
-    </p>
-    <p>
-        The Review Guru
-    </p>
-</div>
+                                {{ $review }}
+                            </div>
+                            <p>
+                                Thank you for using the Two Shakes App.
+                            </p>
+                            <p>Best Regards</p>
+                            <table>
+                                <tr>
+                                    <td>The Review Guru</td>
+                                    <td><img src="https://cdn.mojoimpact.com/twoshakes/reviewguru-xs.webp"
+                                             alt="Useful alt text" width="88" height="128"
+                                             style="border:0; outline:none; text-decoration:none; display:block;"></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <!-- END MAIN CONTENT AREA -->
+                </table>
+                <!-- START FOOTER -->
+                <div class="footer">
+                    <table role="presentation">
+                        <tr>
+                            <td class="content-block">
+                                <span class="apple-link">Designed by Mojo Impact LLC</span>
+                                <br> Don't like these emails? <a href="#">Unsubscribe</a>.
+                            </td>
+                        </tr>
+
+                    </table>
+                </div>
+                <!-- END FOOTER -->
+                <!-- END CENTERED WHITE CONTAINER -->
+            </div>
+        </td>
+        <td>&nbsp;</td>
+    </tr>
+</table>
 </body>
 </html>
