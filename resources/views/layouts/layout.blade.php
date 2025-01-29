08d0ec7b7878f11bb413cc53c1519cea843d8138
Scott Larsen <sslarsen411@gmail.com>
2025-01-29 12:49:07 -0800

    Removed extraneous sweetalert script call

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html p lang="en">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Two Shakes Review | Craft a Great Review in Minutes</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     @vite('resources/css/app.css')
@@ -18,6 +18,5 @@
 @livewireScripts
 @vite('resources/js/app.js')
 @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@11"])
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 </html>
