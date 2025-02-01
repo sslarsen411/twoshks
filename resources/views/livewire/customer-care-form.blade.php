451128003bd635addeab9df889460f38274bd45a
Scott Larsen <sslarsen411@gmail.com>
2025-02-01 13:35:57 -0800

    Fixed guru-icon size and phone field

--- resources/views/livewire/customer-care-form.blade.php
+++ resources/views/livewire/customer-care-form.blade.php
@@ -7,14 +7,14 @@
             <textarea rows="4" wire:model.blur="concerns" x-data="{ name: '{{$concerns}}' }"
                       id="concerns">{{ $concerns }}</textarea>
             @error('concerns') <span class="error">{{ $message }}</span> @enderror
-            <div class="self-start flex items-start my-4">
+            <div class="self-start flex items-start my-4 w-full">
                 <input id="ckCallMe" wire:model.blur="ckCallMe" type="checkbox" value=""
                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2">
-                <label for="ckCallMe" class="ms-2 text-sm font-medium text-gray-900 ">Would you
+                <label for="ckCallMe" class="ms-2 text-xm md:text-sm font-medium text-gray-900 ">Would you
                     like {{ session('location.company') }} to call you?</label>
             </div>
-            <div id="ph_area" class="w-1/2 self-start {{$ckCallMe?'':'hidden'}}">
-                <div class="relative z-0 w-full mb-5 group">
+            <div id="ph_area" class="w-full self-start {{$ckCallMe?'':'hidden'}}">
+                <div class="relative z-0 w-3/4 mb-5 group">
                     <input type="tel" wire:model.live="phone" id="phone" x-mask="(999) 999-9999"
                            class="block py-2.5 px-0 rounded-lg w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 ring-zinc-200 border-gray-300 appearance-none
                      focus:outline-none focus:ring-0 focus:border-blue-600 peer @error('phone') error  @enderror"/>
@@ -23,7 +23,7 @@
                     @error('phone') <span class="error text-xs">{{ $message }}</span> @enderror
                     <label for="phone"
                            class="ml-2 my-2 peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0  peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                        Enter your best phone number:
+                        Phone Number:
                     </label>
                 </div>
             </div>
