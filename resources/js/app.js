360341f212cdeb53bb35b2445a57a4b31829a8f1
Scott Larsen <sslarsen411@gmail.com>
2025-03-17 06:11:34 -0700

    Add universal js sweetalerts

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,11 @@ import './bootstrap';
 
 import.meta.glob([
     '../images/**',
-  ]);
+]);
+window.addEventListener('doAlert', event => {
+    Swal.fire({
+        title: event.detail.title,
+        text: event.detail.text,
+        icon: event.detail.icon
+    })
+});
